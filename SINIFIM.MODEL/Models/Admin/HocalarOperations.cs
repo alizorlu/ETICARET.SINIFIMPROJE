0f112a634a2527500e4c17e16587f0c5e29a1189
İsa Kördiş <isa_kordis21@hotmail.com>
2018-12-16 18:52:34 +0200

    TASK:7-11 # Universiteler Silme, Kodlama

---
+++ SINIFIM.MODEL/Models/Admin/HocalarOperations.cs
@@ -0,0 +1,71 @@
+﻿using SINIFIM.DATABASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
+
+namespace SINIFIM.MODEL.Models.Admin
+{
+  
+        public class HocalarOperations
+        {
+            private SinifimCom_SinifimDB2018DBEntities db { get; set; }
+            public HocalarOperations() => db = new SinifimCom_SinifimDB2018DBEntities();
+
+            public async Task<List<ArgorTB>> GetAllArGorAsync()
+            {
+                return db.ArgorTB.ToList();
+            }
+
+            public async Task<bool> AddArGorAsync(ArgorTB model)
+            {
+                try
+                {
+                    db.ArgorTB.Add(model);
+                    await db.SaveChangesAsync();
+                    return true;
+                }
+                catch (System.Exception)
+                {
+
+                    return false;
+                }
+            }
+
+            public async Task<bool> DeleteArGorAsync(int id)
+            {
+                try
+                {
+                    var finded = await db.ArgorTB.FindAsync(id);
+                    db.ArgorTB.Remove(finded);
+                    db.Entry(finded).State = System.Data.Entity.EntityState.Deleted;
+                    await db.SaveChangesAsync();
+                    return true;
+
+
+                }
+                catch (System.Exception)
+                {
+
+                    return false;
+                }
+
+            }
+            public async Task<ArgorTB> UpdateArGorAsync(ArgorTB model)
+            {
+                db.ArgorTB.AddOrUpdate(model);
+                db.SaveChangesAsync();
+                return model;
+            }
+
+            public async Task<ArgorTB> ReadArGorAsync(int id)
+            {
+                return db.ArgorTB.Find(id);
+
+            }
+
+        }
+    }
+
---
+++ SINIFIM.MODEL/Models/Admin/OgrenciOperations.cs
@@ -0,0 +1,68 @@
+﻿using SINIFIM.DATABASE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
+
+namespace SINIFIM.MODEL.Models.Admin
+{
+    public class OgrenciOperations
+    {
+        private SinifimCom_SinifimDB2018DBEntities db { get; set; }
+        public OgrenciOperations() => db = new SinifimCom_SinifimDB2018DBEntities();
+        public async Task<List<OgrenciTB>> GetAllOgrenciAsync()
+        {
+            return db.OgrenciTB.ToList();
+        }
+        public async Task<bool> AddOgrenciAsync(OgrenciTB model)
+        {
+            try
+            {
+                db.OgrenciTB.Add(model);
+                await db.SaveChangesAsync();
+                return true;
+
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public async Task<bool> DeleteOgrenciAsync(int id)
+        {
+            try
+            {
+                var finded = await db.OgrenciTB.FindAsync(id);
+                db.OgrenciTB.Remove(finded);
+                db.Entry(finded).State = System.Data.Entity.EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return true;
+
+
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public async Task<OgrenciTB> UpdateOgrenciAsync(OgrenciTB model)
+        {
+            db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChangesAsync();
+            return model;
+
+        }
+        public async Task<OgrenciTB> ReadOgrenciTB(int id)
+        {
+            return db.OgrenciTB.Find(id);
+
+        }
+
+    }
+}
