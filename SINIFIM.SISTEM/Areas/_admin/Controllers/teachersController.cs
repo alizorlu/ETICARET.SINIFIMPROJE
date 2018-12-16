24d0bb641bd20e54a6653acf8231541f48d36278
alizorlu <azorlua@gmail.com>
2018-12-16 19:19:25 +0300

    Edit:Sayfalar düzenlemesi

---
+++ SINIFIM.SISTEM/Areas/_admin/Controllers/teachersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SINIFIM.SISTEM.Areas._admin.Controllers
+{
+    public class teachersController : Controller
+    {
+        // GET: _admin/teachers
+        public ActionResult operations()
+        {
+            //Tüm hocalar burada listelenecek
+            return View();
+        }
+        public ActionResult add()
+        {
+            //Burada hoca ekleme sayfası açılacak
+            return View();
+        }
+        
+        
+    }
+}
