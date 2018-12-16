05ca3f2488c6d8e554bedcfea2bc7680916454d9
İsa Kördiş <isa_kordis21@hotmail.com>
2018-12-16 20:53:13 +0200

    Task:13 Ogretim Görevlisi Sayfa Kodlama

--- SINIFIM.SISTEM/Areas/_admin/Controllers/teachersController.cs
+++ SINIFIM.SISTEM/Areas/_admin/Controllers/teachersController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SINIFIM.DATABASE;
+using SINIFIM.MODEL.Models.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,14 @@ namespace SINIFIM.SISTEM.Areas._admin.Controllers
 {
     public class teachersController : Controller
     {
+        private HocalarOperations hocaOps { get; set; } = null;
+        public teachersController() => hocaOps = new HocalarOperations();
         // GET: _admin/teachers
-        public ActionResult operations()
+        public async Task<ActionResult> operations()
         {
             //Tüm hocalar burada listelenecek
-            return View();
+            List<ArgorTB> allList = await hocaOps.GetAllArGorAsync();
+            return View(allList);
         }
         public ActionResult add()
         {
